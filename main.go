890c4d867ad9e369c283be0e64dbeabe3ddf3e05
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-05-09 16:17:53 +0300

    feature: add getting random quiz from file

--- main.go
+++ main.go
@@ -4,19 +4,34 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"time"
 )
 
 func main() {
-	fName := flag.String("f", "quiz.csv", "file name to read quiz from")
+	fileName := flag.String("f", "quiz.csv", "file name to read quiz from")
 	timer := flag.Int("t", 300, "timer in seconds for the quiz")
 	flag.Parse()
 
-	problems, err := problemPuller(*fName)
+	problems, err := problemPuller(*fileName)
 	if err != nil {
 		exit(fmt.Sprintf("Smth went wrong: %s", err.Error()))
 	}
+
+	var quantity int
+	for {
+		fmt.Println("Enter the number of questions you want to quiz:")
+		_, err = fmt.Scanln(&quantity)
+		if err != nil {
+			exit(fmt.Sprintf("Smth went wrong: %s", err.Error()))
+		}
+		if quantity <= len(problems) && quantity > 0 {
+			break
+		}
+	}
+
+	problemList := getRandomProblems(quantity, problems)
 
 	correctAnsw := 0
 
@@ -24,7 +39,7 @@ func main() {
 	ansChan := make(chan string)
 
 problemLoop:
-	for i, p := range problems {
+	for i, p := range problemList {
 		var answer string
 		fmt.Printf("%d: %s=", i+1, p.question)
 
@@ -40,18 +55,17 @@ problemLoop:
 			if isAnswer == p.answer {
 				correctAnsw++
 			}
-			if i == len(problems)-1 {
+			if i == len(problemList)-1 {
 				close(ansChan)
 			}
 		}
 	}
 
-	fmt.Printf("You scored %d out of %d\n", correctAnsw, len(problems))
+	fmt.Printf("You scored %d out of %d\n", correctAnsw, len(problemList))
 	fmt.Println("Press enter to quit")
 }
 
 func problemPuller(filename string) ([]problem, error) {
-
 	if file, err := os.Open(filename); err == nil {
 		csvReader := csv.NewReader(file)
 		if all, err := csvReader.ReadAll(); err == nil {
@@ -62,6 +76,24 @@ func problemPuller(filename string) ([]problem, error) {
 	} else {
 		return nil, fmt.Errorf("error while opening %s: %s", filename, err.Error())
 	}
+}
+
+func getRandomProblems(quantity int, problems []problem) []problem {
+	if quantity >= len(problems) {
+		return problems
+	}
+
+	indexes := make([]int, quantity)
+	for i := 0; i < quantity; i++ {
+		indexes[i] = rand.Intn(len(problems))
+	}
+
+	rndProblems := make([]problem, quantity)
+	for i := 0; i < quantity; i++ {
+		rndProblems[i] = problems[indexes[i]]
+	}
+
+	return rndProblems
 }
 
 func problemParser(lines [][]string) []problem {
